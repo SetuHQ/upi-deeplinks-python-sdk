820380113595cddf8321c2d8114f89da67755874
Nithin <nithin@setu.co>
2020-09-16 16:39:17 +0530

    Update README

--- setu/deeplink.py
+++ setu/deeplink.py
@@ -5,6 +5,7 @@ from .errors import handle_setu_errors
 
 
 class URLS:
+
     class Sandbox:
         url = "https://sandbox.setu.co/api"
 
@@ -14,22 +15,24 @@ class URLS:
 
 class Deeplink:
 
-    def __init__(self, schemeId, secret, productInstance, production=False):
+    def __init__(self, schemeId, secret, productInstance, mode="SANDBOX"):
         self.schemeId = schemeId
         self.secret = secret
         self.productInstance = productInstance
-        self.url = URLS.Sandbox.url if not production else URLS.Prod.url
+        self.url = URLS.Sandbox.url if mode != "PRODUCTION" else URLS.Prod.url
+        self.mode = mode
 
-    def createPaymentLink(
+    # Generate UPI payment link method
+    def create_payment_link(
         self,
         amountValue,
         billerBillID,
         amountExactness,
-        dueDate,
-        expiryDate,
-        payeeName,
-        settlement,
-        validationRules
+        dueDate=None,
+        expiryDate=None,
+        payeeName=None,
+        settlement=None,
+        validationRules=None
     ):
 
         path = "/payment-links"
@@ -39,65 +42,72 @@ class Deeplink:
                 "value": amountValue
             },
             "amountExactness": amountExactness,
-            "billerBillID": billerBillID,
-            "name": payeeName,
-            "dueDate": dueDate,
-            "expiryDate": expiryDate
+            "billerBillID": billerBillID
         }
 
-        if settlement:
+        if payeeName is not None:
+            payload.update({"name": payeeName})
+
+        if dueDate is not None:
+            payload.update({"dueDate": dueDate})
+
+        if expiryDate is not None:
+            payload.update({"expiryDate": expiryDate})
+
+        if settlement is not None:
             payload.update({"settlement": settlement})
 
-        if validationRules:
+        if validationRules is not None:
             payload.update({"validationRules": validationRules})
 
-        if amountExactness == "EXACT_UP":
-            payload["validationRules"] = {
-                "amount": {
-                    "maximum": 0,
-                    "minimum": amountValue
-                }
-            }
-        elif amountExactness == "EXACT_DOWN":
-            payload["validationRules"] = {
-                "amount": {
-                    "maximum": amountValue,
-                    "minimum": 0
-                }
-            }
-
+        # Generate required headers
         headers = generate_setu_headers(
             self.schemeId, self.secret, self.productInstance
         )
+
+        # Call API with required parameters
         response = requests.post(
             self.url + path, json=payload, headers=headers
         )
-        handle_setu_errors(response)
-        data = response.json()
-        self.platformBillID = data["data"]["platformBillID"]
-        return data["data"]
 
-    def checkPaymentStatus(self, platformBillID=None):
-        bill_id = self.platformBillID
-        if platformBillID:
-            bill_id = platformBillID
-        path = "/payment-links/{}".format(bill_id)
+        # Handle errors
+        handle_setu_errors(response)
+
+        return response.json()
+
+    # Check status of UPI payment link method
+    def check_payment_status(
+        self,
+        platformBillID
+    ):
+        path = "/payment-links/{}".format(platformBillID)
+
+        # Generate required headers
         headers = generate_setu_headers(
             self.schemeId, self.secret, self.productInstance
         )
+
+        # Call API with required parameters
         response = requests.get(
             self.url + path, headers=headers
         )
-        data = response.json()
-        print(data)
-        return data["data"]
 
-    def mock_payment(self, amountValue, upiId):
+        return response.json()
+
+    # AVAILABLE ONLY FOR SANDBOX
+    # Trigger mock payment for UPI payment link
+    def trigger_mock_payment(self, amountValue, upiID):
+
+        if self.mode == "PRODUCTION":
+            raise Exception(
+                "trigger_mock_payment METHOD IS IS NOT AVAILABLE IN PRODUCTION"
+            )
+
         path = "/triggers/funds/addCredit"
         payload = {
             "amount": amountValue,
             "destinationAccount": {
-                "accountID": upiId
+                "accountID": upiID
             },
             "sourceAccount": {
                 "accountID": "customer@vpa"
@@ -105,12 +115,14 @@ class Deeplink:
             "type": "UPI",
         }
 
+        # Generate required headers
         headers = generate_setu_headers(
             self.schemeId, self.secret, self.productInstance
         )
+
+        # Call API with required parameters
         response = requests.post(
             self.url + path, json=payload, headers=headers
         )
-        if response.status_code != 200:
-            raise Exception("Failed to mock payment", )
-        return "Mock success"
+
+        return response
