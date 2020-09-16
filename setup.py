0ea3df053ca49da728c1729fbd7002cea321c05b
Nithin <nithin@setu.co>
2020-09-16 13:07:32 +0530

    Housekeeping

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='setu',
-      version='0.4.1',
-      url='https://gitlab.com/setu-lobby/seu-pypi',
+      version='0.4.2',
+      url='https://github.com/SetuHQ/setu-python-sdk',
       author='Gandharva B',
       author_email='gandharva@setu.co',
-      description="Setu's own SDK for faster integration",
+      description="SDK to help Pythonistas integrate with Setu's APIs",
       long_description=README,
       long_description_content_type="text/markdown",
       license='MIT',
