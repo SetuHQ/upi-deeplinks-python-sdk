b1569c5f56abb266cdc82b528220597aea6ac8eb
Naresh <ghostwriternr@gmail.com>
2022-05-27 10:11:08 +0530

    Bump version: 1.0.0 → 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [1.1.0] - 2022-05-27
+
+### Bug fixes
+
+-   Rename SDK import back to setu
+
 ## [1.0.0] - 2022-05-26
 
 -   Major refactor of the SDK
--- setu/__init__.py
+++ setu/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for Setu UPI DeepLinks SDK."""
 from setu.deeplink import Deeplink
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 __all__ = ["Deeplink"]
